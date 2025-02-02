5d9a427260d015fd5894cf977c5406fc82cce71d
baha21730aaaaa <baha1034m@gmail.com>
2025-02-03 01:03:18 +0300

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,17 +1,19 @@
 import tweepy
 
-     # داتاکانی API Keys و Access Tokens
-     API_KEY = 'API_KEY'
-     API_SECRET_KEY = 'API_SECRET_KEY'
-     ACCESS_TOKEN = 'ACCESS_TOKEN'
-     ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'
+# داتاکانی API Keys و Access Tokens
+API_KEY = 'API_KEY'  # لەجیاتی API_KEY، داتاکانی خۆت بنووسە
+API_SECRET_KEY = 'API_SECRET_KEY'  # لەجیاتی API_SECRET_KEY، داتاکانی خۆت بنووسە
+ACCESS_TOKEN = 'ACCESS_TOKEN'  # لەجیاتی ACCESS_TOKEN، داتاکانی خۆت بنووسە
+ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'  # لەجیاتی ACCESS_TOKEN_SECRET، داتاکانی خۆت بنووسە
 
-     # دەستپێکردنی API
-     auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-     api = tweepy.API(auth)
+# دەستپێکردنی API
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
-     # ناردنی توڵێک
-     tweet_text = "ئەمە توڵێکی تاقیکارییە بە بەکارهێنانی Twitter API و Python!"
-     api.update_status(tweet_text)
-
-     print("توڵێک بە سەرکەوتوویی نێردرا!")
+# ناردنی توڵێک
+tweet_text = "ئەمە توڵێکی تاقیکارییە بە بەکارهێنانی Twitter API و Python! 🚀"
+try:
+    api.update_status(tweet_text)
+    print("توڵێک بە سەرکەوتوویی نێردرا! ✅")
+except tweepy.TweepError as e:
+    print(f"هەڵە: {e} ❌")
